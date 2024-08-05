d9059c49da7112c33300ed65e268eff57ee8fb23
jesukka123 <165469028+jesukka123@users.noreply.github.com>
2024-08-05 22:46:22 +0300

    Add files via upload

---
+++ escmap/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+games { 'rdr3', 'gta5' }
+
+author 'Dugget'
+description 'Map Animation Mode'
+version '1.0.0'
+
+client_scripts {
+    'client/*'
+}
+server_scripts {			
+	'server.lua'
+}
